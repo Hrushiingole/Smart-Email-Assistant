43d8f0e2e0b0ab79914d8b8980a3567cd2e0da04
Hrushiingole <142684900+Hrushiingole@users.noreply.github.com>
2025-12-15 15:56:07 +0530

    making use of Mutation observer.

--- Email-Writer-Extension/content.js
+++ Email-Writer-Extension/content.js
@@ -1,1 +1,48 @@
 console.log("Email Writer Extension -Content writer Script");
+
+
+function findComposeToolBar(){
+
+}
+
+function createAIButton(){
+
+}
+
+function injectButton(){
+    const existingButton= document.querySelector('.ai-reply-button');
+    if (existingButton) existingButton.remove();
+
+    const toolbar = findComposeToolBar();
+    if(!toolbar){
+        console.log("toolbar not found");
+        return;
+    }
+    console.log("toolbar found, creating AI button");
+    const button=createAIButton();
+    button.classList.add('ai-reply-button')
+
+    button.addEventListener('click',async()=>{
+
+    });
+    toolbar.insertBefore(button,toolbar.firstChild);
+}
+const observer= new MutationObserver((mutations)=>{
+    for(const mutation of mutations){
+        const addedNodes =Array.from(mutation.addedNodes);
+        const hasComposeElements= addedNodes.some(node=>
+            node.nodeType === Node.ELEMENT_NODE  &&
+            (node.matches('.aDh, .btC, [role="dialog"]') || node.querySelector('.aDh, .btC, [role="dialog"]'))  
+        );
+
+        if(hasComposeElements){
+            console.log('Compose window detected')
+            setTimeout(injectButton,500);
+        }
+    }
+});
+
+observer.observe(document.body,{
+    childList:true,
+    subtree: true
+})
